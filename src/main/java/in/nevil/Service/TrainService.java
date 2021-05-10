8dae00d378862f090145b9507343d66eca5fe0b9
Nevilston <82449413+Nevilston@users.noreply.github.com>
2021-05-10 19:07:19 +0530

    Train List Displayed

---
+++ src/main/java/in/nevil/Service/TrainService.java
@@ -0,0 +1,20 @@
+package in.nevil.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrainService {
+	//Array  initializing 
+	private static Map<String, String> trainNames = new HashMap<String, String>();
+	// Adding Train Name and Train Number 
+	static {
+		trainNames.put("12345M", "Madurai Express");
+		trainNames.put("234451T", "Pearl City Express");
+		trainNames.put("457671C", "Kacheguda Express");
+	}
+	public static Map<String,String>getDisplayTrainName()  {
+		// System.out.println(trainName);// to display the TrainName in Console
+		return trainNames;
+	}
+
+}
---
+++ src/test/java/in/nevil/Service/TrainServicesValidation.java
@@ -0,0 +1,23 @@
+package in.nevil.Service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import in.nevil.Service.TrainService;
+
+public class TrainServicesValidation {
+	//It will display the Number of trains displayed in list is correct 
+	@Test
+	public void displayTrainNames() {
+		Map<String,String>trainNames=TrainService.getDisplayTrainName();
+		assertEquals(3,trainNames.size());
+	}
+	public void displayTrainNames2() {
+		Map<String,String>trainNames=TrainService.getDisplayTrainName();
+		assertNotEquals(5,trainNames.size());
+	}
+}
